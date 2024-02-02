dd0d7d0392e06742e822e1f3b59426e5cf6f62f6
Estellerl <estellerl@icloud.com>
2024-02-02 12:51:26 +0100

    Created explore page, finisihed predict_page

--- predict_page.py
+++ predict_page.py
@@ -17,3 +17,42 @@ def show_predict_page():
     st.title("Software Developer Salary Prediction")
 
     st.write("""### We need some information to predict salary  """)
+
+    countries = (
+       "United States",       
+        "India",      
+      "United Kingdom",    
+       "Germany",   
+       "Canada",   
+       "Brazil", 
+     "France" ,  
+        "Netherlands",
+       "Poland",
+       "Australia",
+      "Spain",
+       "Italy",
+       "Russian Federation", 
+       "Sweden", )
+
+    education = (
+    'Bachelor’s degree',
+    'Master’s degree',
+    'Post grad',
+    'Less than a Bachelors')
+
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education Level", education)
+    experience = st.slider("Years of experience", 0, 50, 3)
+
+    ok = st.button("Calculate Salary")
+    if ok:
+        X = np.array([[country, education, experience ]])
+        X[:, 0] = le_country.transform(X[:,0])
+        X[:, 1] = le_education.transform(X[:,1])
+        X = X.astype(float)
+
+        salary = regressor.predict(X)
+        st.subheader(f"The estimated salary is ${salary[0]:.2f}")
+        
+
+
